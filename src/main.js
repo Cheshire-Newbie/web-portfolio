d832703368b2ee2337fbe34b345f420dba9dd2bc
Officina1 <Officina1@SMA.local>
2022-09-20 22:08:14 +0200

    hosting strony

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
+//importy z zewnetrznych bibliotek
 import Vue from 'vue'
-import App from './components/App/App.vue'
+import { initializeApp } from "firebase/app";
+
+//importy plikow konfiguracyjnych
 import vuetify from './plugins/vuetify'
 import router from './plugins/router'
 
+//to osobno, bo jest to plik ktory bedzie ukryty
+import firebaseConfig from '../Firebase-config'
+
+//wewnetrzne pliki
+import App from './components/App/App.vue'
+
+
 Vue.config.productionTip = false
+
+initializeApp(firebaseConfig)
 
 new Vue({
   vuetify,
